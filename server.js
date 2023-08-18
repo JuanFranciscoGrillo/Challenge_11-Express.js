1d4a78a9caaa05127b781cc8d18f41e748eadd28
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-08-18 08:02:17 -0500

    Deleted Test Note

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
+// Import required modules
+const express = require('express');
+const app = express(); // Create Express app instance
+
+// Import custom route modules
+const api_Routes = require('./routes/ApiRoutes/ApiRoutes');
+const html_Routes = require('./routes/htmlRoutes/htmlRoutes');
+
+// Set the server port
 const PORT = process.env.PORT || 3001;
 
-const express = require('express');
-const app = express();
-
-const fs = require('fs');
-const path = require('path');
-
-
-const api_Routes = require('./routes/ApiRoutes/ApiRoutes');
-const html_Routes = require('./routes/htmlRoutes');
-
-
-app.use(express.urlencoded({
-    extended: true
-}));
-
+// Middleware for parsing data
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// Use API and HTML routes
 app.use('/api', api_Routes);
 app.use('/', html_Routes);
 
+// Start the server
 app.listen(PORT, () => {
-    //debugging
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Server is running on port ${PORT}`);
 });
